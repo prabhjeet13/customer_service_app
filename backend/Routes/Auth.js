7ebe9cae2ae56f9f71eb908511e7395af3c1447d
prabhjeet13 <prabhjeetsingh2000@gmail.com>
2025-01-18 17:00:09 +0530

    completed

--- backend/Routes/Auth.js
+++ backend/Routes/Auth.js
@@ -5,7 +5,6 @@ const passport = require('passport');
 const {googleCallbackControll} = require('../Controllers/Auth');
 
 const REDIRECT_URI = 'https://customer-service-app-z8n9.onrender.com/api/v1/auth/google/callback';
-// router.get('/google',passport.authenticate('google',{scope:['profile','email']}));
 router.get('/google',
     (req,res) => {
         const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.googleid}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email profile`;
